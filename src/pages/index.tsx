9385801ca6585203b08c65ddbde21d6e4782e7b3
KATT <alexander@n1s.se>
2022-11-06 23:47:29 +0100

    woop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { trpc } from '../utils/trpc';
 import { NextPageWithLayout } from './_app';
 import { inferProcedureInput } from '@trpc/server';
 import Link from 'next/link';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import type { AppRouter } from '~/server/routers/_app';
 
 const IndexPage: NextPageWithLayout = () => {
@@ -27,12 +27,12 @@ const IndexPage: NextPageWithLayout = () => {
   });
 
   // prefetch all posts for instant navigation
-  // useEffect(() => {
-  //   const allPosts = postsQuery.data?.pages.flatMap((page) => page.items) ?? [];
-  //   for (const { id } of allPosts) {
-  //     void utils.post.byId.prefetch({ id });
-  //   }
-  // }, [postsQuery.data, utils]);
+  useEffect(() => {
+    const allPosts = postsQuery.data?.pages.flatMap((page) => page.items) ?? [];
+    for (const { id } of allPosts) {
+      void utils.post.byId.prefetch({ id });
+    }
+  }, [postsQuery.data, utils]);
 
   return (
     <>
