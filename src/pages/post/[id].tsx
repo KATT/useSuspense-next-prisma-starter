d3bd75f8ac3d6b08d88f05fee573d492e59f80e4
KATT <alexander@n1s.se>
2022-11-07 23:47:23 +0100

    rm unnecessary crap

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,4 +1,3 @@
-import NextError from 'next/error';
 import { useRouter } from 'next/router';
 import { NextPageWithLayout } from '~/pages/_app';
 import { RouterOutput, trpc } from '~/utils/trpc';
@@ -24,9 +23,7 @@ const PostViewPage: NextPageWithLayout = () => {
   const id = useRouter().query.id as string;
   const postQuery = trpc.post.byId.useSuspenseQuery({ id });
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const data = postQuery.data!;
-  return <PostItem post={data} />;
+  return <PostItem post={postQuery.data} />;
 };
 
 export default PostViewPage;
